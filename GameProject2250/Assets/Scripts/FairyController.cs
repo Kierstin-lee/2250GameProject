aaa1e87ab585d5d411754264cf4bab081f2aafdd
Piperhicks <phicks7@uwo.ca>
2026-03-18 11:21:28 -0400

    made three fairies - M & P

--- GameProject2250/Assets/Scripts/FairyController.cs
+++ GameProject2250/Assets/Scripts/FairyController.cs
@@ -2,15 +2,64 @@ using UnityEngine;
 
 public class FairyController : MonoBehaviour
 {
+
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Vector2 moveInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        UpdateAnimationState();
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (moveInput != Vector2.zero)
+        {
+            rb.MovePosition(rb.position + moveInput.normalized * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    private void UpdateAnimationState()
+    {
+        if (moveInput.x > 0)
+        {
+            anim.SetFloat("MoveX", 1);
+            anim.SetFloat("MoveY", 0);
+        }
+        else if (moveInput.x < 0)
+        {
+            anim.SetFloat("MoveX", -1);
+            anim.SetFloat("MoveY", 0);
+        }
+        else if (moveInput.y > 0)
+        {
+            anim.SetFloat("MoveX", 0);
+            anim.SetFloat("MoveY", 1);
+        }
+        else if (moveInput.y < 0)
+        {
+            anim.SetFloat("MoveX", 0);
+            anim.SetFloat("MoveY", -1);
+        }
+        else
+        {
+            anim.SetFloat("MoveX", 0);
+            anim.SetFloat("MoveY", 0);
+        }
     }
 }
