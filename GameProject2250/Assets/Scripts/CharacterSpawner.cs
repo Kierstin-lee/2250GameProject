f6dbec954072b9cf4ba50fefa0847ca84ca3acf8
emmaghali <eghali@uwo.ca>
2026-03-25 17:59:27 -0400

    fixed camera in main hub to follow selected fairy

--- GameProject2250/Assets/Scripts/CharacterSpawner.cs
+++ GameProject2250/Assets/Scripts/CharacterSpawner.cs
@@ -6,31 +6,43 @@ public class CharacterSpawner : MonoBehaviour
     [SerializeField] private GameObject fairyGreenCharacter;
     [SerializeField] private GameObject fairyOrangeCharacter;
     
+    [SerializeField] private CameraFollow_MainHub cameraFollow;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Selected fairy is: " + GameManager.instance.selectedFairy);
-        
         // deactivate 
         fairyRedCharacter.SetActive(false);
         fairyGreenCharacter.SetActive(false);
         fairyOrangeCharacter.SetActive(false);
+        
+        GameObject selectedObject = null;
         
         // activate selected one
         switch (GameManager.instance.selectedFairy)
         {
             case "FairyR":
                 fairyRedCharacter.SetActive(true);
+                selectedObject = fairyRedCharacter;
                 break;
             case "FairyG":
                 fairyGreenCharacter.SetActive(true);
+                selectedObject = fairyGreenCharacter;
                 break;
             case "FairyO":
                 fairyOrangeCharacter.SetActive(true);
+                selectedObject = fairyOrangeCharacter;
                 break;
-            default:
-                fairyRedCharacter.SetActive(true);  // red fairy as default
+            default: // red fairy as default
+                fairyRedCharacter.SetActive(true);
+                selectedObject = fairyRedCharacter;
                 break;
+        }
+        
+        // assign the camera target
+        if (selectedObject != null && cameraFollow != null)
+        {
+            cameraFollow.target = selectedObject.transform;
         }
     }
 }
