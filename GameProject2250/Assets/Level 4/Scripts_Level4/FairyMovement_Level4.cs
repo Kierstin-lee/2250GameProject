59f727c5da65a36481e13e37ed6267829da0c699
mirapate545 <mpate545@uwo.ca>
2026-03-29 16:14:59 -0400

    Finished Fairy Script based on our level 0 movement script

--- GameProject2250/Assets/Level 4/Scripts_Level4/FairyMovement_Level4.cs
+++ GameProject2250/Assets/Level 4/Scripts_Level4/FairyMovement_Level4.cs
@@ -7,4 +7,46 @@ public class FairyMovement_Level4 : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckRadius = 0.2f;
     [SerializeField] private LayerMask groundLayer;
+    
+    
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
+    private float moveInput;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        if (moveInput < 0)
+            spriteRenderer.flipX = true;
+        else if (moveInput > 0)
+            spriteRenderer.flipX = false;
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck == null) return;
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
 }
