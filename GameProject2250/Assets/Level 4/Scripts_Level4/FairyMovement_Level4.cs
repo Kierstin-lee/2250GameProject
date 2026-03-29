4724ac2870bc182f0e3ad1550a40ce5403c96af8
mirapate545 <mpate545@uwo.ca>
2026-03-29 17:05:55 -0400

    Implementing the level 0 scripts adapted to my level

--- GameProject2250/Assets/Level 4/Scripts_Level4/FairyMovement_Level4.cs
+++ GameProject2250/Assets/Level 4/Scripts_Level4/FairyMovement_Level4.cs
@@ -1,50 +1,93 @@
 using UnityEngine;
 
-public class FairyMovement : MonoBehaviour
+public class FairyController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 10f;
+
+    [Header("Ground Check")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckRadius = 0.35f;
     [SerializeField] private LayerMask groundLayer;
 
     private Rigidbody2D rb;
+    private Animator anim;
     private SpriteRenderer spriteRenderer;
 
-    private float moveInput;
+    private Vector2 moveInput;
     private bool isGrounded;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // If you have a score UI system later, put it back here.
+        // Example:
+        // Score.SetText("Coins: " + score);
     }
 
     void Update()
     {
-        moveInput = Input.GetAxisRaw("Horizontal");
-
-        if (moveInput < 0)
-            spriteRenderer.flipX = true;
-        else if (moveInput > 0)
-            spriteRenderer.flipX = false;
+        moveInput.x = Input.GetAxisRaw("Horizontal");
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
-        Debug.Log("Grounded: " + isGrounded);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
+
+        // Optional: also allow Space to jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+        // Flip sprite left/right
+        if (moveInput.x < 0)
+            spriteRenderer.flipX = true;
+        else if (moveInput.x > 0)
+            spriteRenderer.flipX = false;
+
+        UpdateAnimationState();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
     }
 
-    void OnDrawGizmosSelected()
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Coin"))
+        {
+            Destroy(collision.gameObject);
+
+            // Add score logic here if needed
+            // score++;
+            // Score.SetText("Coins: " + score);
+        }
+    }
+
+    private void UpdateAnimationState()
+    {
+        // Platformer version: only care about horizontal movement + grounded/jumping
+        anim.SetFloat("MoveX", moveInput.x);
+        anim.SetBool("isGrounded", isGrounded);
+
+        if (!isGrounded)
+        {
+            anim.SetBool("isJumping", true);
+        }
+        else
+        {
+            anim.SetBool("isJumping", false);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
     {
         if (groundCheck == null) return;
 
