16f780c6ce2c6b92e24841a2a0595a1682f5ae93
mirapate545 <mpate545@uwo.ca>
2026-03-29 16:12:39 -0400

    creating the fairy movement script

---
+++ GameProject2250/Assets/Level 4/Scripts_Level4/FairyMovement_Level4.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class FairyMovement_Level4 : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+}
