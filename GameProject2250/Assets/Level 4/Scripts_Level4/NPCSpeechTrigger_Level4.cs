348aa8fce429ef3d2ead70b8529b50ab4970613f
mirapate545 <mpate545@uwo.ca>
2026-03-30 13:08:14 -0400

    Working on checkpoint dialogue

---
+++ GameProject2250/Assets/Level 4/Scripts_Level4/NPCSpeechTrigger_Level4.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NPCSpeechTrigger_Level4 : MonoBehaviour
+{
+    public NPCSpeechBubble_Level4 speechBubble;
+    private bool hasSpoken = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!hasSpoken && other.CompareTag("Player"))
+        {
+            speechBubble.ShowFirstDialogue();
+            hasSpoken = true;
+        }
+    }
+}
