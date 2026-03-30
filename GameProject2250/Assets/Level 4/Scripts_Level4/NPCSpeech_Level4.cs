9659b92788e72d8cd6f58adaa3aa159fc1952d96
mirapate545 <mpate545@uwo.ca>
2026-03-30 16:09:49 -0400

    Level 4 change - Making the NPC text disapear

--- GameProject2250/Assets/Level 4/Scripts_Level4/NPCSpeech_Level4.cs
+++ GameProject2250/Assets/Level 4/Scripts_Level4/NPCSpeech_Level4.cs
@@ -4,30 +4,23 @@ using TMPro;
 
 public class NPCSpeech_Level4 : MonoBehaviour
 {
-    [Header("Speech Bubble")]
     [SerializeField] private GameObject speechBubbleObject;
     [SerializeField] private TMP_Text bubbleText;
 
-    [Header("Dialogue")]
     [TextArea] [SerializeField] private string firstLine;
     [TextArea] [SerializeField] private string secondLine;
     [SerializeField] private float timeBetweenLines = 2f;
 
-    private bool hasTriggered = false;
     private Coroutine dialogueRoutine;
 
-    private void Start()
+    void Start()
     {
-        if (speechBubbleObject != null)
-            speechBubbleObject.SetActive(false);
+        speechBubbleObject.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (hasTriggered) return;
         if (!other.CompareTag("Player")) return;
-
-        hasTriggered = true;
 
         if (dialogueRoutine != null)
             StopCoroutine(dialogueRoutine);
@@ -38,7 +31,6 @@ public class NPCSpeech_Level4 : MonoBehaviour
     private IEnumerator ShowDialogueSequence()
     {
         speechBubbleObject.SetActive(true);
-
         bubbleText.text = firstLine;
 
         yield return new WaitForSeconds(timeBetweenLines);
