c076abe9051b092a8ddd9a89989a31f99233ddd8
melejeh <cejeh2@uwo.ca>
2026-03-31 02:59:24 -0400

    Added audio for coin pick up

--- GameProject2250/Assets/Level 1/Scripts_Level1/CoinPickup_Level1.cs
+++ GameProject2250/Assets/Level 1/Scripts_Level1/CoinPickup_Level1.cs
@@ -4,20 +4,21 @@ using TMPro;
 public class CoinPickup_Level1 : MonoBehaviour
 {
     public TMP_Text scoreText;
+    public AudioClip coinSound;
     private int coin = 0;
 
     void Start()
     {
-        scoreText.text = "Coins : 0";
+        scoreText.text = "Coins: 0";
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Hit: " + other.tag);
         if (other.CompareTag("Coin"))
         {
             coin++;
-            scoreText.text = "Coins : " + coin;
+            scoreText.text = "Coins: " + coin;
+            AudioSource.PlayClipAtPoint(coinSound, transform.position);
             Destroy(other.gameObject);
         }
     }
