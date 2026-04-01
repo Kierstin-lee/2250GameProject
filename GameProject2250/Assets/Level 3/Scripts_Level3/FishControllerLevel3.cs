b309f65829ff5e6b8d773a007299568e5ba0ec18
emmaghali <eghali@uwo.ca>
2026-04-01 18:40:16 -0400

    updated and tested fishcontroller script
    fish move,  need to add boundaries

--- GameProject2250/Assets/Level 3/Scripts_Level3/FishControllerLevel3.cs
+++ GameProject2250/Assets/Level 3/Scripts_Level3/FishControllerLevel3.cs
@@ -16,12 +16,44 @@ public class FishControllerLevel3 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // makes it apply to all children?
+        fish = new Transform[transform.childCount];
+        timers = new float[transform.childCount];
+        directions = new int[transform.childCount];
         
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            fish[i] = transform.GetChild(i);
+            directions[i] = Random.value > 0.5f ? 1 : -1; // picks starting direction
+            timers[i] = Random.Range(minDirectionTime, maxDirectionTime); // randomizes times
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < fish.Length; i++)
+        {
+            // moves each fish
+            fish[i].position += Vector3.right * (directions[i] * moveSpeed * Time.deltaTime);
+
+            // count timer
+            timers[i] -= Time.deltaTime;
+
+            if (timers[i] <= 0f)
+            {
+                // flip movement direction
+                directions[i] *= -1;
+
+                // flip sprite horizontally
+                Vector3 scale = fish[i].localScale;
+                scale.x *= -1;
+                fish[i].localScale = scale;
+
+                // reset timer with random duration
+                timers[i] = Random.Range(minDirectionTime, maxDirectionTime);
+            }
+        }
+
     }
 }
