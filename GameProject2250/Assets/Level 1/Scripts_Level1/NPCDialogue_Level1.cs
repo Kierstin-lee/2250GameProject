340718fd923f1b8daea6f5ae38e173d8b92e2754
melejeh <cejeh2@uwo.ca>
2026-03-31 21:20:12 -0400

    edit npc dialogue script

--- GameProject2250/Assets/Level 1/Scripts_Level1/NPCDialogue_Level1.cs
+++ GameProject2250/Assets/Level 1/Scripts_Level1/NPCDialogue_Level1.cs
@@ -1,27 +1,43 @@
 using UnityEngine;
+using TMPro;
 
 public class NPCDialogue_Level1 : MonoBehaviour
 {
     public GameObject speechBubble;
+    public TMP_Text dialogueText;
+
+    private string[] messages = {
+        "Hi! Welcome to the Enchanted Forest!",
+        "Use the arrow keys to move around!",
+        "Press Space to jump!",
+        "Collect coins as you explore!",
+        "Reach the blue portal to go back home!"
+    };
+
+    private int currentMessage = 0;
+    private bool isActive = true;
 
     void Start()
     {
-        speechBubble.SetActive(false);
+        speechBubble.SetActive(true);
+        dialogueText.text = messages[0];
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    void Update()
     {
-        if (other.CompareTag("Player"))
+        if (isActive && Input.GetKeyDown(KeyCode.Space))
         {
-            speechBubble.SetActive(true);
-        }
-    }
+            currentMessage++;
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            speechBubble.SetActive(false);
+            if (currentMessage >= messages.Length)
+            {
+                speechBubble.SetActive(false);
+                isActive = false;
+            }
+            else
+            {
+                dialogueText.text = messages[currentMessage];
+            }
         }
     }
 }
