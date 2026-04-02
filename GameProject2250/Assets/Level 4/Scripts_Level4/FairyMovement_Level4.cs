cb963ff9bd5d4c52300fb1e2f691dccb1b77fedc
Isabel Takacs <105768974+izzytaka@users.noreply.github.com>
2026-04-02 13:10:42 -0400

    up arrow for jump level 4
    Co-Authored-By: emmaghali <231599920+emmaghali@users.noreply.github.com>

--- GameProject2250/Assets/Level 4/Scripts_Level4/FairyMovement_Level4.cs
+++ GameProject2250/Assets/Level 4/Scripts_Level4/FairyMovement_Level4.cs
@@ -35,10 +35,10 @@ public class FairyMovement_Level4 : MonoBehaviour
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-        }
+        //if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        //{
+       //     rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        //}
 
         if (moveInput.x < 0)
             spriteRenderer.flipX = true;
